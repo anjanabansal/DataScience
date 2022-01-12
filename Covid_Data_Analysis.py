dd6c2d531ba9025749835126bc2d2dcf5472d129
anjanabansal <anjana.bansal88@gmail.com>
2022-01-12 21:20:03 +0530

    Remaning analysis questions answered

--- Covid_Data_Analysis.py
+++ Covid_Data_Analysis.py
@@ -31,3 +31,51 @@ print(highOutlier['State/UnionTerritory'].unique())
 #b.rename(columns={"Confirmed":"Outlier"})
 #print(a(['Confirmed'] > (a.loc['75%']['Confirmed'] + iqr)]))
 #print(td.mean(1));
+perMillionFig =  10000000
+
+MergedData = statesData.merge(agg,left_on='State',right_on='State/UnionTerritory')
+print(MergedData);
+MergedData['PerMillion'] = (MergedData['Confirmed']/MergedData['Area (per sq km)'])*perMillionFig;
+print(MergedData['PerMillion']);
+
+print(indiv.columns);
+a = indiv['nationality'].unique();
+print(type(a));
+b = (np.isin(["India","INDIAN"],a));
+print (b);
+
+# condition = a[a.isin(["India","INDIAN"])];
+# print(indiv[condition]);
+
+
+
+#Count the number of males and females.
+numOfMalesAndFemales = indiv['gender'].value_counts();
+print(numOfMalesAndFemales);
+
+a = indiv['detected_state'].groupby(indiv['gender'].value_counts()).count();
+a = indiv['gender'].groupby(indiv['detected_state'].value_counts()).count();
+a= indiv.groupby(['detected_state','gender']).size();
+print(a);
+at = (td['Cured'].groupby(td['State/UnionTerritory'])).sum();
+print((at));
+
+
+# State with maximum recoveries.
+StateWithMaxRecoveries = at.idxmax();
+print(StateWithMaxRecoveries);
+
+
+at1 = (indiv['gender'].groupby(indiv['detected_state'])).value_counts();
+print(at1);
+
+# a = [[1,2],[3,4],[5,6],[7,8]];
+# b = pd.DataFrame((a),index=['a1','a2','a3','n4'],columns = ['c1','c2']);
+# print(b);
+# print(b.a1)
+# print(b[['c1']])
+# print(b[['c1']].filter(like='a')).head();
+# print(b[b[]]);
+d = td.groupby(['Date','Time','Confirmed']).size();
+e = td['Confirmed'].groupby(td['Date']).sum();       
+print(e );
